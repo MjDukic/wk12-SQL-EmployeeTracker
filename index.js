f6671f426ec2fbb3888fe06f03a287b2c1b8df8c
MJ <mirjanadukicc@gmail.com>
2022-12-28 21:59:09 -0800

    setting up more files and getting switch case started

--- index.js
+++ index.js
@@ -2,8 +2,32 @@
 const { prompt } = require("inquirer");
 const db = require("./db/connection");
 
-const start = () => {
+//since using async, we dont have to use then
+//avoid using then keyword
+const start = async () => {
     console.log("Welcome to the Employee Manager!");
+    const { choice } = await prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+
+        }
+    ])
+
+    switch (choice) {
+        case 'View all departments':
+    }
 }
 
 start();
